bfe1bb0ed5899d5a15e69f86c3c84339ee986365
harendra-shakya <harendrashakya125@gmail.com>
2022-09-29 22:06:40 +0530

    initialize

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+
+const App = () => {
+    return (
+        <div className={styles.App}>
+            <div className={styles.container}>
+                <div className="">
+                    <p className={styles.header}>🖼 GIF Portal</p>
+                    <p className={styles.subText}>View your GIF collection in the metaverse ✨</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
